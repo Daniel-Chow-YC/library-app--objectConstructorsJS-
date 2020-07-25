9d47214780947e45962402f4d8a72f47487223df
danielchowyc <daniellchow@gmail.com>
2020-07-25 20:14:06 +0100

    made render function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 // Object Constructor
 function Book(title, author, pages, read) {
     this.title = title
@@ -6,10 +8,30 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-Book.prototype.info = function() {
-    if (this.read) 
-        return `${this.title} by ${this.author}, ${this.pages} pages, has been read`
-    else
-        return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`
+function addBookToLibrary() {
+    let newBook;
+    myLibrary.push(newBook);
 }
 
+// creating books
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+const hp = new Book("Harry Potter", "Jk Rowling", 300, false)
+// adding books to myLibrary
+myLibrary.push(theHobbit);
+myLibrary.push(hp);
+
+let table = document.getElementById("myTable");
+
+function render() {
+    for (let i=0; i < myLibrary.length; i++) {
+        let row = table.insertRow(i+1);
+        row.insertCell(0).innerHTML = `${myLibrary[i].title}`
+        row.insertCell(1).innerHTML = `${myLibrary[i].author}`
+        row.insertCell(2).innerHTML = `${myLibrary[i].pages}`
+        if (myLibrary[i].read) 
+            row.insertCell(3).innerHTML = `Yes`;
+        else
+            row.insertCell(3).innerHTML = `No`;
+    }
+}
+render();
